797710ab46033c26190bb28a0528b5287498035a
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2024-04-07 17:02:21 +0200

    chore: add @CrossOrigin annotations in controllers

--- src/main/java/dev/bogdanjovanovic/jwtsecurity/auth/AuthController.java
+++ src/main/java/dev/bogdanjovanovic/jwtsecurity/auth/AuthController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin
 public class AuthController {
 
   private static final Logger log = LoggerFactory.getLogger(AuthController.class);
--- src/main/java/dev/bogdanjovanovic/jwtsecurity/demo/DemoController.java
+++ src/main/java/dev/bogdanjovanovic/jwtsecurity/demo/DemoController.java
@@ -2,12 +2,14 @@ package dev.bogdanjovanovic.jwtsecurity.demo;
 
 import dev.bogdanjovanovic.jwtsecurity.exception.ApiResponseWrapper;
 import java.security.Principal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo")
+@CrossOrigin
 public class DemoController {
 
   @GetMapping
